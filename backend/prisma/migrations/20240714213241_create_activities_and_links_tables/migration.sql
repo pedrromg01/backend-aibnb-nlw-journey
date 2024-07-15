c39b90e7d434082bfbdd90231604adddf88af9c2
Pedro Martins Cabral de Almeida <pedro.almeida@oititec.com.br>
2024-07-15 12:47:16 -0300

    first commit

---
+++ backend/prisma/migrations/20240714213241_create_activities_and_links_tables/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - You are about to drop the `participantes` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "participantes";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "participants" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "is_owner" BOOLEAN NOT NULL DEFAULT false,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "participants_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "activities" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "accurs_at" DATETIME NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "activities_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "links" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "links_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
